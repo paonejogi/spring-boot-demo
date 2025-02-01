3ab0db1cac5ad7fa67d38323f6fdd12e9fbc5662
paonejogi <paonejogi@gmail.com>
2025-02-01 20:16:23 +0530

    Web Services Implementation

---
+++ src/main/java/com/example/springbootdemo/controller/HomeController.java
@@ -0,0 +1,30 @@
+package com.example.springbootdemo.controller;
+
+import com.example.springbootdemo.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+//@Controller
+//@ResponseBody
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String Greetings(){
+        return "Hello there!";
+    }
+
+//    @RequestMapping(value ="/user", method = RequestMethod.GET)
+    @GetMapping("/user")
+    public User user(){
+        User user = new User();
+        user.setId("1");
+        user.setName("Pavan");
+        user.setEmail("paonejogi@gmail.com");
+        return user;
+    }
+
+    @GetMapping("/{id}/{id1}")
+    public String userInfo(@PathVariable String id, @PathVariable("id1") String name){
+        return "This is user id : " +id + " and his name is :  " + name;
+    }
+}
